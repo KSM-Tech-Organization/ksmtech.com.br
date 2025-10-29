eea28b5c964c5676d9d78651c74e76c4d9d330e4
KennysSparda <kennykysv@gmail.com>
2025-10-29 02:43:40 -0300

    feat(base): add initial page and npm scripts

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to KSM Tech</h1>
+        <p>Your gateway to tech solutions.</p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
