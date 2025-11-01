225f0901716b16462f5020825b2e74d37bb484be
KennysSparda <kennykysv@gmail.com>
2025-11-01 14:32:30 -0300

    feat(ads): add google tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,41 @@
 import Head from "next/head";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+
+    // pageview inicial + debug
+    useEffect(() => {
+        if (typeof window !== "undefined" && window.gtag)
+            window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
+                page_path: window.location.pathname,
+                debug_mode: true,
+            });
+    }, []);
+
+    // pageview nas trocas de rota do Next
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            if (typeof window !== "undefined" && window.gtag)
+                window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
+                    page_path: url,
+                });
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () =>
+            router.events.off("routeChangeComplete", handleRouteChange);
+    }, [router.events]);
+
     return (
         <>
             <Head>
                 <title>KSM Tech</title>
             </Head>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
         </>
     );
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <title>KSM Tech</title>
             </Head>
             <body>
                 <Main />
